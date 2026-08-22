48cba0467d4df5338a93d65dc773ab884ef8a54f
pkaso903-ted <pkaso903@gmail.com>
2026-08-23 00:10:51 +0900

    add config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// 네이버 클라우드 플랫폼 Maps > Application "noodle-map" 의 Client ID
+// 서비스 URL 등록: https://pkaso903-ted.github.io , http://localhost:8899
+// 값을 비우면 OpenStreetMap으로 자동 대체됩니다.
+window.NOODLE_CONFIG = {
+  naverKeyId: 'nfpmiyc876'
+};
